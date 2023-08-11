9abdde9f6a52e4c735cec416a315b5e073850fc5
jeongdonggi <dkrhank45@gmail.com>
2023-08-11 21:21:48 +0900

    first

---
+++ jeong/src/test/java/reactinjava/jeong/JeongApplicationTests.java
@@ -0,0 +1,13 @@
+package reactinjava.jeong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
