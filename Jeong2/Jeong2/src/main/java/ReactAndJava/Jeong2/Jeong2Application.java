71956aac049f62512487659018248debd35926ae
jeongdonggi <dkrhank45@gmail.com>
2023-08-13 01:02:32 +0900

    recreate Jeong2

---
+++ Jeong2/Jeong2/src/main/java/ReactAndJava/Jeong2/Jeong2Application.java
@@ -0,0 +1,13 @@
+package ReactAndJava.Jeong2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jeong2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jeong2Application.class, args);
+	}
+
+}
---
+++ Jeong2/Jeong2/src/test/java/ReactAndJava/Jeong2/Jeong2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ReactAndJava.Jeong2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jeong2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
